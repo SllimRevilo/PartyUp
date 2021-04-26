39237cfedd70c3417040a78b5e15f5723dede758
Oliver Mills <mills.oliver@ymail.com>
2021-04-26 13:19:16 -0400

    entry point changed

--- PartyUp/src/app/app-routing.module.ts
+++ PartyUp/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    //loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'login', pathMatch: 'full' // makes default to login page instead of the home page
   },
   {
     path: 'login',
