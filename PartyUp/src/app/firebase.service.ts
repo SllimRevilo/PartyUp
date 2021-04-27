1744bb3188dd13e42410f9b416e100463032b0fd
Bennett Marra <bmarra@email.sc.edu>
2021-04-26 20:36:31 -0400

    Add functional login check

--- PartyUp/src/app/firebase.service.ts
+++ PartyUp/src/app/firebase.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
 //import {Item,Order} from '../modal/Item';
 import {AngularFirestore, AngularFirestoreCollection, DocumentReference} from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 import {map, take} from 'rxjs/operators';
 import firebase from 'firebase/app';
 
@@ -10,5 +11,16 @@ import firebase from 'firebase/app';
 })
 export class FirebaseService {
 
+  uid='';
+
   constructor(private afs: AngularFirestore) { }
+
+  setUID(uid){
+    this.uid=uid;
+    console.log(this.uid);
+    }
+  
+    getUserID(){
+    return this.uid
+    }
 }
--- PartyUp/src/app/login/login.page.ts
+++ PartyUp/src/app/login/login.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
+import {AngularFirestore } from '@angular/fire/firestore';
+import {FirebaseService} from '../firebase.service';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +14,55 @@ export class LoginPage implements OnInit {
 
   user= {email:"username@gmail.com", password:"password"}
 
-  constructor(private router:Router) { }
+  constructor(public afAuth: AngularFireAuth, 
+  	private fbService: FirebaseService,
+    public firebase:AngularFirestore,
+    private router:Router) { }
 
   ngOnInit() {
   }
 
   login(email: string, password: string)
   {
-    this.router.navigate(["home"]);
+  	// Promise<firebase.auth.UserCredential>
+    console.log("signin ...");
+    this.afAuth.signInWithEmailAndPassword(email, password).then(user => {
+      //navigate to user profile
+      console.log(user.user.email, user.user.uid);
+      //this.fbService.load_my_orders();
+  
+      var user1 = firebase.auth().currentUser;
+      console.log(user1.uid)
+      this.fbService.setUID(user.user.uid);
+      // fbService
+      var db = firebase.firestore();
+      var self=this;
+      db.collection("usertype").where("uid", "==", user1.uid)
+            .get()
+            .then(function(querySnapshot) {
+                querySnapshot.forEach(function(doc) {
+                    // doc.data() is never undefined for query doc snapshot
+                    console.log(doc.id, " => ", doc.data());
+                    //var type = doc.data().usertype;
+                    //console.log("usertype:"+type);
+                    //self.fbService.setUsertype(type);
+                    // if(type == 'owner') {
+                    //   self.fbService.isOwner = true;
+                    // }
+                });
+            })
+            .catch(function(error) {
+                console.log("Error getting documents: ", error);
+            });
+            //this.router.navigate(["/product-list"])
+            //this.router.navigate(["/"])
+            this.router.navigateByUrl('/home'); //might need to use navigateByURL('/') workaround for speed
+    })
+    .catch(error => {
+      console.log(error)
+    });
+    //this.fbService.showLogin=false
+    
   }
 
   signUp()
